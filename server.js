baa50a5130a79db5bcab8d5da530ba44152c0a39
zaiton <cietonz@gmail.com>
2022-01-01 00:45:30 +0800

    initial commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-149yur-h9cida'));
+app.use(express.static('./dist/moh-nyoping'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-149yur-h9cida/'}),
+    res.sendFile('index.html', {root: 'dist/moh-nyoping/'}),
 );
 
 // Start the app by listening on the default Heroku port
