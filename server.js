a5f18d6076f3ba987eb433c5e935e6648910fc94
zaiton <cietonz@gmail.com>
2022-01-01 02:00:55 +0800

    initial commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/appku2'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/aappku2/'}),
+    res.sendFile('index.html', {root: 'dist/appku2/'}),
 );
 
 // Start the app by listening on the default Heroku port
